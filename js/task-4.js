1357e3c7a73b908c21d69001ef400e99c5bdf189
Oleksii Kovsh <kovsh1999@gmail.com>
2025-07-23 16:58:07 +0300

    complete fourth task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (
+    event.target.elements.email.value.trim() === '' ||
+    event.target.elements.password.value.trim() === ''
+  ) {
+    alert('All form fields must be filled in');
+    return;
+  }
+  console.log({
+    [event.target.elements.email.name]:
+      event.target.elements.email.value.trim(),
+    [event.target.elements.password.name]:
+      event.target.elements.password.value.trim(),
+  });
+  form.reset();
+}
